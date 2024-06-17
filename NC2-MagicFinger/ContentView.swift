b740aac6847902451fc1f6840d2c4aee0edcf542
Jonghyeon Chang <hokhmah91@naver.com>
2024-06-17 12:11:59 +0900

    project 생성

---
+++ NC2-MagicFinger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NC2-MagicFinger
+//
+//  Created by Chang Jonghyeon on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NC2-MagicFinger/NC2_MagicFingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC2_MagicFingerApp.swift
+//  NC2-MagicFinger
+//
+//  Created by Chang Jonghyeon on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NC2_MagicFingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
